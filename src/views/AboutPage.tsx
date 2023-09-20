60d7fed0c4a9a3a1adc387a3e9234249f5da8b48
ronaldoro <ronaldoro>
2023-09-20 23:02:54 +0900

    phase1

---
+++ src/views/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AboutPage.css';
+import { useScroll } from "framer-motion"
+
+function AboutPage() {
+  return (
+    <div className="centered-image-container">
+    </div>
+  );
+}
+
+
+
+export default AboutPage;
